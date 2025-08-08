9b072d7faaef0264f79786c81a2718a546a0c8f8
Michelle Massaad <michelle.massaad@epita.fr>
2025-08-08 13:41:04 +0200

    done

---
+++ not_operator.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<stdbool.h>
+
+int main(){
+
+    bool sunny =true;
+
+    if(!sunny)
+    {
+        printf("\nIt's cloudy outside!");
+    }
+
+    else
+    {
+        printf("\n It's sunny outside!");
+    }
+
+
+
+    return 0;
+}
+
